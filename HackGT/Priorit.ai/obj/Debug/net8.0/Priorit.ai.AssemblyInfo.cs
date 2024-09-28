245dcc424a5deb30125d71167792e697774fe829
Krissh Bhargava <49103625+krisshbhargava@users.noreply.github.com>
2024-09-28 04:23:02 -0400

    additions to homepage

--- HackGT/Priorit.ai/obj/Debug/net8.0/Priorit.ai.AssemblyInfo.cs
+++ HackGT/Priorit.ai/obj/Debug/net8.0/Priorit.ai.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Priorit.ai")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ed3bcbb53efb23231ac246b4db790f359e0952")]
 [assembly: System.Reflection.AssemblyProductAttribute("Priorit.ai")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Priorit.ai")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
